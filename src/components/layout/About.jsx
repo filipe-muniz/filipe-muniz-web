6c2b3b07476fddb7fe1b41e94163e4842db632ec
filipe <77029323+filipe-muniz@users.noreply.github.com>
2025-03-23 10:01:33 -0300

    small correction

--- src/components/layout/About.jsx
+++ src/components/layout/About.jsx
@@ -3,7 +3,7 @@ const About = () => {
     <section className="container mx-auto max-w-5xl px-4 ">
       <h2 className="text-3xl font-bold text-gray-800 mb-6">About Me</h2>
       <p className="text-lg text-gray-700 leading-relaxed">
-      I'm a Computer Science student from Brazil with experience in full-stack development, working primarily with React.js and Node.js. In college, I was part of a research team exploring Large Language Models (LLMs) for sentiment analysis in Natural Language Processing (NLP).
+      I'm a Computer Science student from Brazil with experience in full-stack development, working primarily with React.js and Java. In college, I was part of a research team exploring Large Language Models (LLMs) for sentiment analysis in Natural Language Processing (NLP).
       </p>
       <p className="text-lg text-gray-700 leading-relaxed ">
         I'm passionate about interdisciplinary learning and enjoy integrating ideas from various fields. My interests include Music, Computer Science, Web Development, Mathematics, and Philosophy. Currently, I'm exploring functional programming.
